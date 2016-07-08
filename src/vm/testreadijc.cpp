fd600bcdac1122fe1a241ec016830a1a3a48587e
Graham Markall <graham.markall@embecosm.com>
2016-07-08 19:05:44 +0100

    Add simple test of read ijc

--- src/vm/testreadijc.cpp
+++ src/vm/testreadijc.cpp
@@ -1,19 +1,44 @@
+#include <iostream>
+
 #include "cardapplet.h"
 #include "installer.h"
 #include "readijc.h"
 
-void
+using namespace std;
+
+int
 testReadIJC(char* ijcFileName)
 {
-	unsigned char *dataBuffer = new unsigned char[10000];
-	int iDataLength = readBin(ijcFileName, dataBuffer);
-	//outputHex(dataBuffer,iDataLength);
-	CardApplet* capp = (CardApplet*) buildApplet(dataBuffer, iDataLength);
-	capp->displayInfo();
+  unsigned char *dataBuffer = new unsigned char[10000];
+  int len = readBin(ijcFileName, dataBuffer);
+
+  if (len == -1)
+  {
+    cout << "Error in readBin()" << endl;
+    return 1;
+  }
+  //outputHex(dataBuffer,iDataLength);
+  CardApplet* capp = (CardApplet*) buildApplet(dataBuffer, len);
+  if (capp->pHeader == NULL)
+  {
+    cout << "Error in buildApplet()" << endl;
+    return 1;
+  }
+
+  // Could check some of this info later on
+  //capp->displayInfo();
+
+  return 0;
 }
 
 int
 main(int argc, char **argv)
 {
-  return 0;
+  if (argc < 2)
+  {
+    cout << "Usage: test_readijc <path to ShortAdd.ijc>" << endl;
+    exit(2);
+  }
+
+  return testReadIJC(argv[1]);
 }
