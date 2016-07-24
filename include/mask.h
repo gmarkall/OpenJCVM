e55a3d0418e50c60d6534d3ec16052291a7ca035
Graham Markall <graham.markall@embecosm.com>
2016-07-24 21:44:42 +0100

    Add copyright header to all files

--- include/mask.h
+++ include/mask.h
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2008 - present Oliver Lusing / Louis Liu
+ *
+ * This file is part of OpenJCVM; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation version 2.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * OpenJCVM; see the file COPYING. If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef mask_h_
 #define mask_h_
 
